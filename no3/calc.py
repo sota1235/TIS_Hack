ff3820d3d82ee8cd2fe49530b343692c7ac5d938
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-02-27 17:01:36 +0900

    掛け算完成

---
+++ no3/calc.py
@@ -0,0 +1,17 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#def minus(a, b):
+
+
+def times(a, b):
+    ans = 0
+    for i in range(b):
+        ans += a
+    return ans
+
+#def define(a, b):
+
+
+# debug
+print times(3, 4)
