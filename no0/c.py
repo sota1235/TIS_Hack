fd2a8bd2555599aa71f6013d5143ac38cd59a427
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-02-27 11:45:56 +0900

    fix c.py / とりあえず完成

--- no0/c.py
+++ no0/c.py
@@ -1,2 +1,31 @@
 # /usr/bin/python
 # -*- coding: utf-8 -*-
+
+import sys
+
+def isSubstring(s1, s2):
+    s1 = list(s1)
+    s2 = list(s2)
+
+    # 文字数が違う場合
+    if len(s1) != len(s2):
+        print "false"
+        sys.exit()
+
+    while(len(s1) != 0):
+        index = s2.index(s1[0])
+        i = 0
+        while(s1[index+0] == s2[index+0]):
+            s1.pop(index+0)
+            s2.pop(index+0)
+        if len(s1) == 1:
+            if s1[0] == s2[0]:
+                print "true"
+            else:
+                print "false"
+            sys.exit()
+
+s1_d = sys.argv[1]
+s2_d = sys.argv[2]
+
+isSubstring(s1_d, s2_d)
