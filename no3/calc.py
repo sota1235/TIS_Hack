161ccb836cc5720e54952f2a2ef2208d7858f086
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-02-27 17:10:49 +0900

    割り算完成

--- no3/calc.py
+++ no3/calc.py
@@ -1,17 +1,24 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#def minus(a, b):
+# def minus(a, b):
 
 
-def times(a, b):
+def multiply(a, b):
     ans = 0
     for i in range(b):
         ans += a
     return ans
 
-#def define(a, b):
-
+# 小数バージョン
+def devide(a, b):
+    ans = 0
+    rest = 0
+    while b < a:
+        ans += 1
+        b += b
+    return ans
 
 # debug
-print times(3, 4)
+print multiply(3, 4)
+print devide(15, 3)
