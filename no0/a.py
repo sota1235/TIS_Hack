bbcb8701dc202eb078881daabc60570bb4a8b000
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-02-27 11:23:00 +0900

    fix a.py / 一問目完成(要改良)

--- no0/a.py
+++ no0/a.py
@@ -1,2 +1,18 @@
 # /usr/bin/python
 # -*- coding: utf-8 -*-
+
+import sys
+
+argv = list(sys.argv[1])
+count = 0
+
+for s in argv:
+    for i in range(len(argv)):
+        if s == argv[i]:
+            count += 1
+    if count > 1:
+        print "false"
+        sys.exit()
+    count = 0
+
+print "true"
