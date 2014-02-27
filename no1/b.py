ee8539d022650ab39d1879f1a8ba72d1b00ac172
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-02-27 13:32:58 +0900

    add b.py / たぶん完成

---
+++ no1/b.py
@@ -0,0 +1,25 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Queue:
+
+    def __init__(self,stac):
+        self.stac1 = stac
+        self.stac2 = stac
+        self.leng = len(stac)
+
+    def add(obj):
+        stac1.queue.append(obj)
+        stac2.queue.append(obj)
+
+    def T_peek():
+        return self.stac1(0)
+
+    def T_remove
+        s = self.stac(0)
+        self.stac1.pop(0)
+        self.stac2.pop(0)
+        return s
+
+    def size():
+        return leng
