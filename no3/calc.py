b1c386de7292fc434b9780c8730aca299b3d1738
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-02-27 17:56:50 +0900

    fix calc.py / バグ修正

--- no3/calc.py
+++ no3/calc.py
@@ -18,12 +18,28 @@ def multiply(a, b):
 # integral number ver
 def devide(a, b):
     ans = 0
+    bb = b
     while b < a:
         ans += 1
-        b += b
+        b += bb
+    return ans
+
+# decimal number ver
+def devide_d(a, b):
+    ans = 0
+    bb = b
+    while b < a:
+        ans += 1
+        b += bb
+    b -= bb
+    # 小数第一位
+    while b < a:
+        ans += 0.1
+        b += multiply(0.1, bb)
     return ans
 
 # debug
 print "3 * 4 = " + str(multiply(3, 4))
 print "15 / 3 = " + str(devide(15, 3))
 print "15 - 18 = " + str(minus(15, 18))
+print "5 / 2 = " + str(devide_d(5, 2))
