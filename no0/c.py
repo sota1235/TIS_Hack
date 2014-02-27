59e246f0e4c511781defc34e54dc485da3d42f72
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-02-27 13:00:11 +0900

    fix c.py / 問題文の意図どおり

--- no0/c.py
+++ no0/c.py
@@ -3,36 +3,28 @@
 
 import sys
 
+# b.pyより
 def isSubstring(s1, s2):
-    s1 = list(s1)
-    s2 = list(s2)
-
-    # 文字数が違う場合
-    if len(s1) != len(s2):
-        print "false"
+    l1 = list(s1)
+    l2 = list(s2)
+    count = 0
+    # s2の一文字目がなかったら終了
+    if not l2[0] in l1:
         sys.exit()
-
-    while(len(s1) != 0):
-        if s1[0] in s2:
-            index = s2.index(s1[0])
-        else:
-            print "false"
-            sys.exit()
-        i = 0
-        while(s1[0] == s2[index] and len(s1) != 1):
-            s1.pop(0)
-            s2.pop(index)
-            if index == len(s2):
+    while(l2[0] in l1):
+        index = l1.index(l2[0])
+        for i in range(len(l2)):
+            if not l1[index+i] == l2[i]:
                 break
-        if len(s1) == 1:
-            if s1[0] == s2[0]:
+            if i+1 == len(l2):
                 print "true"
-            else:
-                print "false"
-            sys.exit()
+                sys.exit()
+        l1.pop(index)
+    print "false"
+    sys.exit()
 
-# for debug
+# デバッグ用
 s1_d = sys.argv[1]
 s2_d = sys.argv[2]
 
-isSubstring(s1_d, s2_d)
+isSubstring(s1_d+s1_d, s2_d)
