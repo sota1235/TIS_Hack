118be5c5c6fd5c01e9665a7144e26990ebca6702
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-02-28 14:19:55 +0900

    fix garden.py / やりかけだけど発表資料作るので離脱

--- no5/garden.py
+++ no5/garden.py
@@ -8,10 +8,26 @@ def make_shower(n,xyr):
     # xが小さい順にsort
     xyr.sort()
     half = math.floor(len(xyr)/2)
-    xyr1 = xyr[:int(half)]
-    xyr2 = xyr[int(half):]
-    print xyr1
-    print xyr2
+
+    xyr1 = xyr[:half]
+    xyr2 = xyr[half:]
+    # 新しい円の要件を入れる配列
+    new_xyr1 = []
+    new_xyr2 = []
+    for i in range(len(xyr1)-1,2):
+        x = (xyr[i][0] + xyr[i+1][0]) / 2
+        y = (xyr[i][1] + xyr[i+1][1]) /2
+        r = get_length(xyr[i][0],xyr[i][1],xyr[i+1][0],xyr[i+1][0])
+        box = [x,y,r]
+        new_xyr1.append(box)
+
+"""
+2つ円を囲む最小の円のxyrを作り
+それが1になるまで繰り返す
+"""
+
+def get_length(x1,y1,x2,y2):
+    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
 # input
 """
