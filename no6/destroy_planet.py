d41914a4b99f7fe922563b572478c5437703a4c8
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-02-28 10:41:49 +0900

    fix destroy_planet.py / x,y座標をarrayに格納 | inputの型修正

--- no6/destroy_planet.py
+++ no6/destroy_planet.py
@@ -1,7 +1,16 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 
+def beam(n,array):
+    x_list = [0 for i in range(n)]
+    y_list = [0 for i in range(n)]
+    for i in range(n):
+        x_list[i] = array[i][0]
+        y_list[i] = array[i][1]
+    print x_list
+    print y_list
 
 # input
 print "Enter N"
@@ -21,4 +30,13 @@ for i in range(len(RC)):
     RC[i] = RC[i].replace(')', '')
     RC[i] = RC[i].rstrip(',')
     RC[i] = RC[i].split(',')
+    RC[i] = map(int ,RC[i])
+
+# エラーチェック
+if len(RC) != int(K):
+    print "Error: 座標の数とKの値が一致しません"
+    sys.exit()
+
 print RC
+
+beam(int(K), RC)
