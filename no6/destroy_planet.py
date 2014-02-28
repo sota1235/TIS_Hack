5449817a4c4910b5e20ddd0bacf8366c98a17d0c
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-02-28 11:28:03 +0900

    fix destroy_planet.py / 一応完成

--- no6/destroy_planet.py
+++ no6/destroy_planet.py
@@ -4,8 +4,12 @@
 import sys
 
 def beam(n,k,array):
+    # param
     x_list = [0 for i in range(n)]
     y_list = [0 for i in range(n)]
+    beam_t = 0
+    beam_array = []
+    planet_array = array
 
     # make list of x,y
     for i in range(k):
@@ -14,13 +18,48 @@ def beam(n,k,array):
     print x_list
     print y_list
 
+    # 発射する列,行を決定
+    while(sum(x_list) != 0 and sum(y_list) != 0):
+        flag = 0 # 0:x 1:y
+        box = []
+        if max(x_list) < max(y_list):
+            index = y_list.index(max(y_list))
+            y_list[index] = 0
+            flag = 1
+        else:
+            index = x_list.index(max(x_list))
+            x_list[index] = 0
+            flag = 0
+        print index
+        print flag
+        print planet_array
+        for i in range(len(planet_array)):
+            print i
+            print flag
+            print planet_array[i][flag]
+            if planet_array[i][flag] == index:
+                box.append(planet_array[i])
+                planet_array[i] = [n+1, n+1]
+        if box != []:
+            beam_array.append(box)
+            beam_t += 1
+    print beam_array
+    print beam_t
+
 # input
+"""
 print "Enter N"
 N = raw_input()
 print "Enter K"
 K = raw_input()
 print "Enter (R,C)"
 RC = raw_input()
+"""
+
+# debug
+N = 3
+K = 4
+RC = "{(0,0),(0,2),(1,1),(2,1)}"
 
 # 座標データ整形
 RC = RC.lstrip('{')
