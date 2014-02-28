8b1e2d98b3291f862ff9607679e7217874fa9add
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-02-28 12:04:10 +0900

    fix destroy~ / 整形

--- no6/destroy_planet.py
+++ no6/destroy_planet.py
@@ -42,30 +42,26 @@ def beam(n,k,array):
     print "必要なビーム発射回数: " + str(beam_t)
 
 # input
+"""
 print "Enter N"
 N = raw_input()
 print "Enter K"
 K = raw_input()
 print "Enter (R,C)"
 RC = raw_input()
+"""
 
 # debug
-"""
 N = 3
 K = 4
 RC = "{(0,0),(0,2),(1,1),(2,1)}"
-"""
 
 # 座標データ整形
-RC = RC.lstrip('{')
-RC = RC.rstrip('}')
+RC = RC[2:-1]
 RC = RC.split('(')
-RC.pop(0)
 for i in range(len(RC)):
-    RC[i] = RC[i].replace(')', '')
-    RC[i] = RC[i].rstrip(',')
-    RC[i] = RC[i].split(',')
-    RC[i] = map(int ,RC[i])
+    RC[i] = RC[i][0:3]
+    RC[i] = map(int, RC[i].split(','))
 
 # エラーチェック
 if len(RC) != int(K):
