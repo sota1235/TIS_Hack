c7341a73a9e82dc460333501a3e789c83b46e304
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-02-28 14:57:10 +0900

    fix garden.py / ２つのグループを１つの円まで絞り込むとこまで完成

--- no5/garden.py
+++ no5/garden.py
@@ -7,19 +7,30 @@ import math
 def make_shower(n,xyr):
     # xが小さい順にsort
     xyr.sort()
-    half = math.floor(len(xyr)/2)
+    half = int(math.floor(len(xyr)/2))
 
     xyr1 = xyr[:half]
     xyr2 = xyr[half:]
     # 新しい円の要件を入れる配列
-    new_xyr1 = []
-    new_xyr2 = []
-    for i in range(len(xyr1)-1,2):
-        x = (xyr[i][0] + xyr[i+1][0]) / 2
-        y = (xyr[i][1] + xyr[i+1][1]) /2
-        r = get_length(xyr[i][0],xyr[i][1],xyr[i+1][0],xyr[i+1][0])
-        box = [x,y,r]
-        new_xyr1.append(box)
+    if len(xyr1) != 1:
+        new_xyr1 = get_newxyr(xyr1)
+    else:
+        new_xyr1 = xyr1
+    if len(xyr2) != 1:
+        new_xyr2 = get_newxyr(xyr2)
+    else:
+        new_xyr2 = xyr2
+
+    # 生成
+    while 1:
+        if len(new_xyr1) == 1:
+            break
+        new_xyr1 = get_newxyr(new_xyr1)
+    while get_newxyr(new_xyr2):
+        new_xyr2 = get_newxyr(new_xyr2)
+
+    print new_xyr1
+    print new_xyr2
 
 """
 2つ円を囲む最小の円のxyrを作り
@@ -28,6 +39,16 @@ def make_shower(n,xyr):
 
 def get_length(x1,y1,x2,y2):
     return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+def get_newxyr(xyr):
+    res = []
+    for i in range(0,len(xyr)-1,2):
+        x = (xyr[i][0] + xyr[i+1][0]) / 2
+        y = (xyr[i][1] + xyr[i+1][1]) / 2
+        r = get_length(xyr[i][0],xyr[i][1],xyr[i+1][0],xyr[i+1][0])
+        box = [x,y,r]
+        res.append(box)
+    return res
 
 # input
 """
