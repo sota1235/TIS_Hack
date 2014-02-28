b4868e3ec58fb1d828d7906676bceed95810680e
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-02-28 13:51:21 +0900

    配列の２分割に成功

--- no5/garden.py
+++ no5/garden.py
@@ -2,6 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import itertools
+import math
+
+def make_shower(n,xyr):
+    # xが小さい順にsort
+    xyr.sort()
+    half = math.floor(len(xyr)/2)
+    xyr1 = xyr[:int(half)]
+    xyr2 = xyr[int(half):]
+    print xyr1
+    print xyr2
 
 # input
 """
@@ -11,11 +21,11 @@ print "(X, Y, R) = "
 XYR = raw_input()
 """
 # debug
-XYR = "{(20,10,2),(20,20,2),(40,10,3)}"
+XYR = "{(20,10,2),(10,20,2),(40,10,3)}"
 
 # 整形
 XYR = XYR[2:-1].split('(')
 for i in range(len(XYR)):
     XYR[i] = map(int, XYR[i][0:7].split(','))
 
-print XYR
+make_shower(3,XYR)
