63502dceccdae8cef99de6b85358faa2d596d77d
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-02-28 09:51:31 +0900

    fix calc.py / Finish

--- no3/calc.py
+++ no3/calc.py
@@ -1,6 +1,8 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+
 def minus(a, b):
     ans = 0
     flag = 'a' if a > b else 'b'
@@ -42,30 +44,34 @@ def devide_d(a, b):
     return ans
 
 # debug
+"""
 print "3 * 4 = " + str(multiply(3, 4))
 print "15 / 3 = " + str(devide(15, 3))
 print "15 - 18 = " + str(minus(15, 18))
 print "5 / 2 = " + str(devide_d(5, 2))
+"""
 
 # 発表用
 while 1:
     print "please operation"
     print "ex) 3 + 7"
-    str = list(raw_input())
-    if len(str) != 3:
+    s = raw_input().split()
+    if s[0] == "exit": sys.exit()
+    if len(s) != 3:
         print "Error: 書式が違います"
         continue
-    a = int(str[0]), b = int(str[2])
-    calc = str[1]
+    a = int(s[0])
+    b = int(s[2])
+    calc = s[1]
     if calc == "+":
         ans = a + b
-    else if calc == "-":
+    elif calc == "-":
         ans = minus(a,b)
-    else if calc == "*":
+    elif calc == "*":
         ans = multiply(a,b)
-    else if calc == "/":
+    elif calc == "/":
         ans = devide(a,b)
     else:
         print "Error: 記号が無効です"
         continue
-    print "answer : " + ans
+    print "answer : " + str(ans)
