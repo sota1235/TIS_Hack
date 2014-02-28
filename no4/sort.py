4b2ffdec6bc1ec62876be494993f270a770be383
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-02-28 10:04:15 +0900

    fix sort.py / 動いてるが挙動がおかしい

--- no4/sort.py
+++ no4/sort.py
@@ -3,24 +3,24 @@
 
 def main(array):
     old = array
-    new = array.sort()
+    array.sort()
     m = 0
     n = 0
 
     # mを求める
     for i in range(len(old)):
-        if old[i] != new[i]:
+        if old[i] != array[i]:
             m = i
             old = old[:i+1]
-            new = new[:i+1]
+            array = array[:i+1]
             break
     # nを求める
     for j in range(len(old)):
-        if old[-i+1] != new[-i+1]:
+        if old[-i+1] != array[-i+1]:
             n = m + len(old) - i
             break
-    print "m="+m
-    print "n="+n
+    print "m="+str(m)
+    print "n="+str(n)
 
 # debug
 test = [1,2,4,7,10,11,7,12,8,16,18,19]
